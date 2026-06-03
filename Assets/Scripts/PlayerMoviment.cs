98fa19ebe9f32591df25a969de3f9d294390ef38
André Lopes <alglopes@id.uff.br>
2026-06-02 22:17:43 -0300

    [feat] Player Moviment
    - Inicio do movimento do player
    - Ainda esta muito "cru", precisa polir o movimento e detectar colisao com parede
    - Acho que o movimento fica mais rapido/devagar dependendo da distancia
    - A camera precisa seguir o jogador

---
+++ Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    public float velocidade = 5f;
+    
+    private Vector2 destino;
+    private Camera mainCamera;
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+    private void OnMove(InputAction.CallbackContext contexto)
+    {
+        if (contexto.started)
+        {
+            Vector2 mousePos = Mouse.current.position.ReadValue();
+            destino = mainCamera.ScreenToWorldPoint(mousePos);
+        }
+    }
+    private void Update()
+    {
+        if ((Vector2)transform.position != destino)
+        {
+            // MoveTowards move de um ponto A para um ponto B em linha reta, numa velocidade constante
+            transform.position = Vector2.MoveTowards(transform.position, destino, velocidade * Time.deltaTime);
+        }
+    }
+}
