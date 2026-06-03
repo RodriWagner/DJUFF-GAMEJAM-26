918c13ff6f8c29fd43e6d9b6f0d193984a597406
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-06-03 10:38:14 -0300

    [feat] layers e manager de relaidade
    -inicio do realityManager
    -layers pra cada mundo

---
+++ Assets/Scripts/RealityManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+public class RealityManager : MonoBehaviour
+{
+    public static RealityManager Instance; //instanciar o script
+    public enum RealityType { BlackAndWhite, Colorful }
+    public RealityType currentReality = RealityType.BlackAndWhite;
+    public UnityEvent onRealityChanged;
+
+    void Start()
+    {
+        //configurar a instancia
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    private void ChangeReality(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            //logica de inversao
+            if (currentReality == RealityType.BlackAndWhite)
+                currentReality = RealityType.Colorful;
+            else
+                currentReality = RealityType.BlackAndWhite;
+
+            //invoke da troca de realidade (avisar globalmente)
+            if (onRealityChanged != null)
+                onRealityChanged.Invoke();
+        }
+
+    }
+
+}
