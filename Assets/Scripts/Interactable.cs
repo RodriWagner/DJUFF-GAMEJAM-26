77bf25dbb808de05e0324a7f309ee2f8ca1118af
André Lopes <alglopes@id.uff.br>
2026-06-03 11:28:28 -0300

    [update] Tipos de Interacao
    - Existem 3 formas diferentes nas quais o objeto interaje com o jogo
    - Provavelmente precisa separar as 3, cada uma em um script

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -3,9 +3,21 @@ using UnityEngine.InputSystem;
 
 public class Interactable : MonoBehaviour
 {
-    public bool Interactive;
+    [Tooltip("Objeto muda algo na fase")] public bool Interactive;
+    [Tooltip("Objeto mostra um texto na tela")] public bool Informative;
+    [Tooltip("Texto a ser mostrado")] public string message;
+    [Tooltip("Objeto pode ser ampliado")] public bool Zoom;
     public void action()
     {
         Debug.Log("Fiz algo");
+    }
+    public void text()
+    {
+        Debug.Log("Mostrei algo na tela");
+        Debug.Log(message);
+    }
+    public void zoom()
+    {
+        Debug.Log("ZOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOM");
     }
 }
