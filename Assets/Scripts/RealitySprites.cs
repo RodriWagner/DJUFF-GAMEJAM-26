7c3f4f5b705aefe5987040c334707842887b95d4
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-06-03 16:57:09 -0300

    [feat/fix] troca de material para sprite
    -os itens que mudam de textura nao serao mais tratados com materiais, e sim com SPRITES
    -obs: criei sprites aleatorios {horriveis feiosos) pra testar a mudanca

---
+++ Assets/Scripts/RealitySprites.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+
+public class RealitySprites : MonoBehaviour
+{
+    //OBJETOS QUE EXISTEM EM AMBAS AS REALIDADES, MAS QUE ALGO MUDA NELES
+    [Header("Sprite das Realidades")]
+    public Sprite spriteBlackAndWhite;
+    public Sprite spriteColorful;
+    private SpriteRenderer myRenderer;
+    void Start()
+    {
+        myRenderer = GetComponent<SpriteRenderer>();
+        if (RealityManager.Instance != null)
+        {
+            RealityManager.Instance.onRealityChanged.AddListener(Updatesprite);
+        }
+        Updatesprite();
+    }
+    void Updatesprite()
+    {
+        if (myRenderer != null)
+        {
+            if (RealityManager.Instance.currentReality == RealityManager.RealityType.BlackAndWhite)
+                myRenderer.sprite = spriteBlackAndWhite;
+            else
+                myRenderer.sprite = spriteColorful;
+        }
+    }
+}
