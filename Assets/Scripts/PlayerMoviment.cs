b48dc66a5a72ccab26e97aec66c96ef80736577c
André Lopes <alglopes@id.uff.br>
2026-06-06 12:34:29 -0300

    [update] Controles
    - Removido botao direito
    - O personagem anda ate o objeto que deseja interagir e, se estiver no alcance, interage
    - As vezes nao funciona
    - Codigo modularizado

--- Assets/Scripts/PlayerMoviment.cs
+++ Assets/Scripts/PlayerMoviment.cs
@@ -9,6 +9,7 @@ public class PlayerMoviment : MonoBehaviour
     public Vector2 destino;
     private Camera mainCamera;
     public bool canMove = true;
+    private bool moving = false;
 
     [SerializeField] private float interaction_range = 3.5f;
 
@@ -18,8 +19,26 @@ public class PlayerMoviment : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        MovePlayer();
+        if (moving)
+        {
+            DetectCollision();
+        }
+    }
+    private void OnInteract(InputAction.CallbackContext contexto)
+    {
+        if (contexto.started)
+        {
+            if (!canMove) return;
+            Vector2 mousePos = Mouse.current.position.ReadValue();
+            destino = mainCamera.ScreenToWorldPoint(mousePos);
+        }
+    }
+    private void MovePlayer()
+    {
         if (RealityManager.Instance.cooldownTime > 0.0f)
         {
+            moving = false;
             return;
         }
         //MOVIMENTA O PLAYER
@@ -27,52 +46,47 @@ public class PlayerMoviment : MonoBehaviour
         {
             // MoveTowards move de um ponto A para um ponto B em linha reta, numa velocidade constante
             transform.position = Vector2.MoveTowards(transform.position, destino, velocidade * Time.deltaTime);
+            moving = true;
+        }
+        else moving = false;
+    }
+    private void DetectCollision()
+    {
+        Vector2 MousePos = Mouse.current.position.ReadValue();
+        Ray raio = mainCamera.ScreenPointToRay(MousePos);
+        RaycastHit2D hit = Physics2D.GetRayIntersection(raio); //detecta que o mouse esta por cima de algum objeto
+        if (hit.collider != null) //detecta que o objeto tem um colisor
+        {
+            DetectRange(hit.collider);
         }
     }
-    private void OnInteract(InputAction.CallbackContext contexto)
+    private void DetectRange(Collider2D target)
     {
-        if (contexto.started)
+        float obj_distance = Vector2.Distance(transform.position, target.transform.position);
+        if(obj_distance > interaction_range)
         {
-            Vector2 MousePos = Mouse.current.position.ReadValue();
-            Ray raio = mainCamera.ScreenPointToRay(MousePos);
-            RaycastHit2D hit = Physics2D.GetRayIntersection(raio); //detecta que o mouse esta por cima de algum objeto
-            if (hit.collider != null) //detecta que o objeto tem um colisor
+            Debug.Log("Objeto muito longe para ser interagido.");
+            return;
+        }
+        DoAction(target);
+    }
+    private void DoAction(Collider2D target)
+    {
+        if (target.gameObject.TryGetComponent<Interactable>(out Interactable ActionObject)) //detecta se o objeto e o mesmo que tem o script
+        {
+            if (ActionObject.interactive) ActionObject.Action();
+            if (ActionObject.informative) ActionObject.ShowText();
+            if (ActionObject.zoom)
             {
-                float obj_distance = Vector2.Distance(transform.position, hit.collider.transform.position);
-                if(obj_distance > interaction_range)
-                {
-                    Debug.Log("Objeto muito longe para ser interagido.");
-                    return;
-                }
-                Debug.Log("colidi!");
-                if (hit.collider.gameObject.TryGetComponent<Interactable>(out Interactable ActionObject)) //detecta se o objeto e o mesmo que tem o script
-                {
-                    Debug.Log("WOW O SCRIPT");
-                    if (ActionObject.interactive) ActionObject.Action();
-                    if (ActionObject.informative) ActionObject.ShowText();
-                    if (ActionObject.zoom)
-                    {
-                        ActionObject.Amplify();
-                        canMove = false;
-                    }
-                }
-                //USANDO SO PRA PORTA
-                if (hit.collider.gameObject.TryGetComponent<WindowClose>(out WindowClose Script))
-                {
-                    Script.Close();
-                    canMove = true;
-                }
+                ActionObject.Amplify();
+                canMove = false;
             }
         }
-    }
-
-    private void OnMove(InputAction.CallbackContext contexto)
-    {
-        if (!canMove) return;
-        if (contexto.started)
+        //USANDO SO PRA PORTA
+        if (target.gameObject.TryGetComponent<WindowClose>(out WindowClose Script))
         {
-            Vector2 mousePos = Mouse.current.position.ReadValue();
-            destino = mainCamera.ScreenToWorldPoint(mousePos);
+            Script.Close();
+            canMove = true;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
