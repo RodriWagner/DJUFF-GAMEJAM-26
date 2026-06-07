1aeb6dde266fe88b772f5396431c8debdea1fdb9
Wuogui <rodrigo.wag.s@gmail.com>
2026-06-07 18:30:33 -0300

    feat(Scripts): AudioManager
    - Implementado um audio manager que encapsula as funções de som.
    - O uso em scripts é por instancia, o que facilita o usar sem precisa importar nada e abstrai as funções.
    - Facilita regular o som de cada tipo de som diferente com os Busses
    - Falta colocar os caminhos corretos dos Bus

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,62 @@
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+    // isso é pras musicas que loopam (como as de fundo):
+    private EventInstance musicInstance;
+
+    // Bus para regular os volumes
+    private Bus generalBus;
+    private Bus musicBus;
+    private Bus effectsBus;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        generalBus = RuntimeManager.GetBus("bus:/");
+        musicBus = RuntimeManager.GetBus("bus:/");
+        effectsBus = RuntimeManager.GetBus("bus:/");
+    }
+
+    public void SetGeneralVolume(float volume)
+    {
+        generalBus.setVolume(volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicBus.setVolume(volume);
+    }
+
+    public void SetEffectsVolume(float volume)
+    {
+        effectsBus.setVolume(volume);
+    }
+
+    public void PlayOneShot(EventReference soundEvent, Vector3 soundPosition)
+    {
+        RuntimeManager.PlayOneShot(soundEvent, soundPosition);
+    }
+
+    public void StartMusic(EventReference musicEvent)
+    {
+        musicInstance = RuntimeManager.CreateInstance(musicEvent);
+        musicInstance.start();
+    }
+
+    public void StopMusic()
+    {
+        musicInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        musicInstance.release(); // isso  libera a memoria
+    }
+}
