9e0de59023323a7de5f8fa405eb5bf268ac88dcb
André Lopes <alglopes@id.uff.br>
2026-06-10 14:28:19 -0300

    [feat] Botoes
    - Sprite dos botoes adicionados

--- Assets/Scripts/WindowClose.cs
+++ Assets/Scripts/WindowClose.cs
@@ -2,18 +2,13 @@ using UnityEngine;
 
 public class WindowClose : MonoBehaviour
 {
-    [Tooltip("Manager")] public GameObject pai;
-    [Tooltip("Manager")] public GameObject pai2;
+    [Tooltip("Manager")] public GameObject pai; 
     [Tooltip("Player")] public GameObject player;
     public void Close()
     {
         if (pai.TryGetComponent<Interactable>(out Interactable ActionObject))
         {
-            if (pai2.TryGetComponent<Interactable>(out Interactable ActionObject2))
-            {
-                ActionObject.ExitZoom();
-                ActionObject2.ExitZoom();
-            }
+            ActionObject.ExitZoom();
             if (player.TryGetComponent<PlayerMoviment>(out PlayerMoviment script))
             {
                 script.canMove = true;
