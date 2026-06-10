1dc2564d6a985d2733925a9ae82b16715098dae1
André Lopes <alglopes@id.uff.br>
2026-06-10 13:53:34 -0300

    [fix] Botao de interacao

--- Assets/Scripts/WindowClose.cs
+++ Assets/Scripts/WindowClose.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class WindowClose : MonoBehaviour
 {
     [Tooltip("Manager")] public GameObject pai;
+    [Tooltip("Manager")] public GameObject pai2;
     [Tooltip("Player")] public GameObject player;
     public void Close()
     {
         if (pai.TryGetComponent<Interactable>(out Interactable ActionObject))
         {
-            ActionObject.ExitZoom();
+            if (pai2.TryGetComponent<Interactable>(out Interactable ActionObject2))
+            {
+                ActionObject.ExitZoom();
+                ActionObject2.ExitZoom();
+            }
             if (player.TryGetComponent<PlayerMoviment>(out PlayerMoviment script))
             {
                 script.canMove = true;
