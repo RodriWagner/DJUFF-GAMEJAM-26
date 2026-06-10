d0dc01185ed625ab614d40df336105fc482dd6e7
André Lopes <alglopes@id.uff.br>
2026-06-09 22:16:45 -0300

    [Update] Organizacao
    - Reorganizei os gameobjects em pastas
    - Iniciei a implementacao dos sons do livro

---
+++ Assets/Scripts/BookSounds.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel;
+using FMODUnity;
+using UnityEngine;
+
+public class BookSounds : MonoBehaviour
+{
+    [Header("Sons a serem tocados")]
+    [Tooltip("DO (C)")] public EventReference One;
+    [Tooltip("RE (D)")] public EventReference Two;
+    [Tooltip("MI (E)")] public EventReference Three;
+    [Tooltip("FA (A)")] public EventReference Four;
+    [Tooltip("SOL (G)")] public EventReference Six;
+    public void Sound1()
+    {
+        
+    }
+    public void Sound2()
+    {
+        
+    }
+    public void Sound3()
+    {
+        
+    }
+    public void Sound4()
+    {
+        
+    }
+    public void Sound5()
+    {
+        
+    }
+}
