340d6431bb8caa6200e362fe4383235abbcdb215
André Lopes <alglopes@id.uff.br>
2026-06-10 09:21:05 -0300

    [Update] Sala 1 colorida

--- Assets/Scripts/RealityPlayer.cs
+++ Assets/Scripts/RealityPlayer.cs
@@ -13,7 +13,6 @@ public class RealityPlayer : MonoBehaviour
         if (RealityManager.Instance != null)
         {
             RealityManager.Instance.onRealityChanged.AddListener(UpdateObject);
-            OnRealityChanged();
         }
     }
 
