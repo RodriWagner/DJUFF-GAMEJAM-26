bc7f789877e9cd91637d2678bc1cceecdc83844e
Wuogui <rodrigo.wag.s@gmail.com>
2026-06-10 10:17:06 -0300

    feat(BookSoundManager): Sons presentes no livro

--- Assets/Scripts/BookSounds.cs
+++ Assets/Scripts/BookSounds.cs
@@ -5,29 +5,29 @@ using UnityEngine;
 public class BookSounds : MonoBehaviour
 {
     [Header("Sons a serem tocados")]
-    [Tooltip("DO (C)")] public EventReference One;
-    [Tooltip("RE (D)")] public EventReference Two;
-    [Tooltip("MI (E)")] public EventReference Three;
-    [Tooltip("FA (A)")] public EventReference Four;
-    [Tooltip("SOL (G)")] public EventReference Six;
+    [Tooltip("Som do livro 1")] public EventReference One;
+    [Tooltip("Som do livro 2")] public EventReference Two;
+    [Tooltip("Som do livro 3")] public EventReference Three;
+    [Tooltip("Som do livro 4")] public EventReference Four;
+    [Tooltip("Som do livro 5")] public EventReference Five;
     public void Sound1()
     {
-        
+        AudioManager.Instance.PlayOneShot(One, transform.position);
     }
     public void Sound2()
     {
-        
+        AudioManager.Instance.PlayOneShot(Two, transform.position);
     }
     public void Sound3()
     {
-        
+        AudioManager.Instance.PlayOneShot(Three, transform.position);
     }
     public void Sound4()
     {
-        
+        AudioManager.Instance.PlayOneShot(Four, transform.position);
     }
     public void Sound5()
     {
-        
+        AudioManager.Instance.PlayOneShot(Five, transform.position);
     }
 }
