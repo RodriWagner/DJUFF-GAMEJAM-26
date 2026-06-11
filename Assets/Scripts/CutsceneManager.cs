f68ff516720d11b3369b8c60d79f8847f593208d
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-06-10 21:52:22 -0300

    [feat] cutscene funfando

--- Assets/Scripts/CutsceneManager.cs
+++ Assets/Scripts/CutsceneManager.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class CutsceneManager : MonoBehaviour
 {
-
+    public PlayerMoviment playerMoviment;
+    public GameBGMusic bGMusic;
+    private VideoPlayer cutscene;
     void Start()
     {
-        
+        cutscene = GetComponent<VideoPlayer>();
+        cutscene.loopPointReached += endCutscene;
+        playerMoviment.enabled = false;
     }
 
+    void endCutscene(VideoPlayer vp)
+    {
+        gameObject.SetActive(false);
+        bGMusic.FirstStartBGMusic();
+        playerMoviment.enabled = true;
+    }
 }
